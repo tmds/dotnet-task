38b36530ffedbdc7714168dfd57475f9ba333a04
Tom Deseyn <tom.deseyn@gmail.com>
2024-11-05 13:19:50 +0100

    Add dotnetstreams chart

--- create-helm-repo.sh
+++ create-helm-repo.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-rm -rf charts
+#rm -rf charts
 
-mkdir charts
-helm package src -d charts
-helm repo index charts --url https://raw.githubusercontent.com/tmds/dotnet-task/refs/heads/main/charts
+#mkdir charts
+#helm package src -d charts
+rm charts/index.yaml
+helm repo index charts --url https://raw.githubusercontent.com/tmds/dotnet-task/refs/heads/streams/charts
